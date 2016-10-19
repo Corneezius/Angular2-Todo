221571248248349931f78315bdbf8506450abd45
Cornelius Range <cfr2116@columbia.edu>
2016-10-18 20:35:33 -0400

    update newtask component

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,25 @@
-import { NgModule }       from '@angular/core';
-import { BrowserModule }  from '@angular/platform-browser';
-import { AppComponent }   from './app.component';
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+import { AppComponent }  from './app.component';
+import { PiesListComponent } from './pies-list.component';
+import { TaskListComponent }   from './task-list.component';
+import { EditTaskComponent }   from './edit-task.component';
+import { NewTaskComponent }  from './new-task.component';
 
-@NgModule ({
-  imports:      [BrowserModule],
-  declarations: [AppComponent],
-  bootstrap:     [AppComponent]
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  declarations: [
+    PiesListComponent,
+    AppComponent,
+    TaskListComponent,
+    EditTaskComponent,
+    NewTaskComponent
+  ],
+  bootstrap:    [ AppComponent ]
 })
 
 export class AppModule { }
